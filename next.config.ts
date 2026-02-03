d33b6281835f06e20a51280505b9644a88315d61
muratkanik <mkanik@gmail.com>
2026-02-04 00:22:33 +0300

    fix: add source.unsplash.com to next.config

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
