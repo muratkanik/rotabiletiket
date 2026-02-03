0b081c2664fdc3ff97ff1da7990cb0fc34636104
muratkanik <mkanik@gmail.com>
2026-02-04 00:20:05 +0300

    fix: add plus.unsplash.com to next.config

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
